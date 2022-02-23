3edac3d2452be79e649b09c6b07605bdd2ba2779
Mark <3376246739@qq.com>
2022-02-23 21:42:19 +0800

    first commit

---
+++ src/main/java/com/lanzhiqing/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.lanzhiqing.git;
+
+/**
+ * @author Jack
+ * @description:
+ * @date 2022/2/23 21:38
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
